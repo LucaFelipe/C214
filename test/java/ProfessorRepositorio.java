7c5a53de3262dd36dfb6e7c8e3dc131c211793a1
LucaFelipe <99219082+LucaFelipe@users.noreply.github.com>
2025-04-04 20:18:14 -0300

    5 testes de sucesso, 5 testes de falha e pom.xml

---
+++ test/java/ProfessorRepositorio.java
@@ -0,0 +1,5 @@
+public class ProfessorRepositorio {
+    public String buscarHorarioProfessor(String nomeProfessor) {
+        throw new UnsupportedOperationException("Método deve ser mockado nos testes.");
+    }
+}
