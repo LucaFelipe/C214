8c8ee6689ee860886416801c9c18330770ad68ee
LucaFelipe <luca.felipe@ges.inatel.br>
2025-04-04 23:04:55 -0300

    atualizando o pom.xml

--- test/java/ProfessorRepositorio.java
+++
@@ -1,5 +0,0 @@
-public class ProfessorRepositorio {
-    public String buscarHorarioProfessor(String nomeProfessor) {
-        throw new UnsupportedOperationException("Método deve ser mockado nos testes.");
-    }
-}
